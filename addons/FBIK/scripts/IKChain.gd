703ab5f1ff401dd77a09c689de9d5c3a9420142b
R3X-G1L6AME5H <r3x_g1l6ame5h@protonmail.com>
2022-10-11 12:18:43 +0000

    Stamp&Document - IK Bone

--- addons/FBIK/scripts/IKChain.gd
+++ addons/FBIK/scripts/IKChain.gd
@@ -1,11 +1,31 @@
 tool
 extends Position3D
 
+
+"""
+	FBIKM - Chain
+   		by Nemo Czanderlitch/Nino Čandrlić
+			@R3X-G1L       (godot assets store)
+			R3X-G1L6AME5H  (github)
+
+	This node HAS to be a child of a FBIKM node to work. This node itself is the target.
+
+	Name "Chain" comes from the fact that it solves for a set of connected bones; they solve a part of a skeleton;
+    you can imagine your arm as a chain of two bones: elbow, and forearm. It begins its solving process from the tip
+	bone, and continues up the tree structure until it reaches the user specified root bone, or a bone without a parent.
+
+	Because FBIKM allows for multi chain solving, each chain has its individual target, and the pull force. When two
+	chains pull at one another, priority is decided by by this value.
+"""
+
+
 const FBIKM_NODE_ID = 1  # THIS NODE'S INDENTIFIER
 
 var tip_bone_id : String = "-1"
 var root_bone_id : String = "-1"
 export (float, 0.005, 1.0) var pull_strength = 1
+
+## BOILERPLATE FOR DROPDOWN MENUS ##
 var _bone_names = "VOID:-1"
 
 func _get(property : String):
@@ -16,6 +36,7 @@ func _get(property : String):
 			return root_bone_id 
 		_ :
 			return null
+
 func _set( property : String, value) -> bool:
 	match property:
 		"tip_bone_id" :
@@ -26,6 +47,7 @@ func _set( property : String, value) -> bool:
 			return true
 		_ :
 			return false
+
 func _get_property_list():
 	var result = []
 	result.push_back({
@@ -44,15 +66,18 @@ func _get_property_list():
 	})
 	return result
 
-###############################################################################
+## INIT ###########################################################################
 func _ready():
 	if Engine.editor_hint:
 		if get_parent().get("FBIKM_NODE_ID") == 0:  ## This is KinematicsManager's ID
+			## This way the parent notifies this node whenever there is a change in the bone structure
 			get_parent().connect("bone_names_obtained", self, "_update_parameters")
 
+## Update the dropdown menu
 func _update_parameters( bone_names : String ) -> void:
 	self._bone_names = bone_names
 	property_list_changed_notify()
 
+## Return current position (used by the solver)
 func get_target() -> Transform:
 	return self.transform
